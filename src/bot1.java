d65afb4cef9a49ed2fb872e30ed4311e448f9a01
Aaron <38624405+NotAaronMa@users.noreply.github.com>
2018-11-22 13:18:03 -0500

    Update bot1.java

--- src/bot1.java
+++ src/bot1.java
@@ -1,5 +1,3 @@
-
-
 import hlt.*;
 import java.util.*;
 
@@ -15,20 +13,16 @@ public class bot1 {
             game.updateFrame();
             game.endTurn(master.update(game));
         }
-
     }
 }
-
 class Master{
     private int[][]ph;
-
     public Master(Game g){
         int mw = g.gameMap.width;
         int mh = g.gameMap.height;
 
         ph = new int[mw][mh];
     }
-
     public LinkedList<Command> update(Game game) {
         LinkedList<Command> commands = new LinkedList<>();
         for (int i = 0; i < ph.length; i++) {
@@ -38,18 +32,10 @@ class Master{
                 }
             }
         }
-
-
         LinkedList<Ship> myShips = new LinkedList<Ship>();
         for (final Ship ship : game.me.ships.values()) {
             myShips.push(ship);
         }
-        return commands;
-
+        return commands;   
     }
-
 }
-
-
-
-
