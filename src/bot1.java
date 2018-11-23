49e42b6f22d177dfbbd6ba18a6c307ecd0f3882b
Aaron <38624405+NotAaronMa@users.noreply.github.com>
2018-11-23 13:43:03 -0500

    Update bot1.java

--- src/bot1.java
+++ src/bot1.java
@@ -1,7 +1,9 @@
+
+
 import hlt.*;
 import java.util.*;
 
-public class bot1 {
+public class MyBot {
     static Game game;
     static boolean shouldClose = false;
     static Master master;
@@ -13,16 +15,23 @@ public class bot1 {
             game.updateFrame();
             game.endTurn(master.update(game));
         }
+
     }
 }
+
+//manages ships and large scale operations
 class Master{
     private int[][]ph;
+    
+    //maps ships to slaves
+    private Map<Ship, Slave> slaves;
     public Master(Game g){
         int mw = g.gameMap.width;
         int mh = g.gameMap.height;
 
         ph = new int[mw][mh];
     }
+
     public LinkedList<Command> update(Game game) {
         LinkedList<Command> commands = new LinkedList<>();
         for (int i = 0; i < ph.length; i++) {
@@ -32,10 +41,41 @@ class Master{
                 }
             }
         }
-        LinkedList<Ship> myShips = new LinkedList<Ship>();
+
+
         for (final Ship ship : game.me.ships.values()) {
-            myShips.push(ship);
+            if(slaves.get(ship) == null){
+                slaves.put(ship, new Slave(ship));
+            }
+            Slave s = slaves.get(ship);
+            s.update();
         }
-        return commands;   
+        
+        
+        return commands;
+
     }
+}
+
+//wrapper for ship stores a directive location and an ai
+class Slave{
+    Ship s;
+    
+    public Slave(Ship s){
+        this.s = s;
+    }
+    
+
+}
+
+//regular pair class like really whytf would i not be able to get this
+
+class Pair{
+    private int a,b;
+    public Pair(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+
 }
